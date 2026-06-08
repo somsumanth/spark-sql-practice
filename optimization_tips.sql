b7732b5dd9018d1e7fd226c670492eceb270ec10
Somasundar M <97184928+somsumanth@users.noreply.github.com>
2026-06-08 23:22:26 +0530

    Create optimization_tips.sql

---
+++ optimization_tips.sql
@@ -0,0 +1,11 @@
+-- Spark SQL Optimization Tips
+
+-- Use partitioning for large datasets
+
+-- Use broadcast joins for small tables
+
+-- Filter records early
+
+-- Avoid SELECT *
+
+-- Cache frequently used DataFrames
